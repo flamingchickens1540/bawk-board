4c5a791d64947b6eed9ac003f377302f9f600b86
Zach R <zarutman+git@gmail.com>
2022-11-29 18:21:29 -0800

    update example secrets

--- secrets.example.js
+++ secrets.example.js
@@ -1,2 +1,4 @@
 export const origin_url = "https://githubaccount:ghp_abcdefghijklmnopqrstuvwxyz@github.com/username/repository"
 export const auth_secret = "chickens!"
+export const tba_secret_id = "abcd"
+export const tba_secret = "efghijklmnopqrstuvwxyz"
